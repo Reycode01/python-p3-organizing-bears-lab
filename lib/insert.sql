f0ed5080f1ce6dd37aee3f3161deb3b23b3c99d3
humphrey <writernic89@gmail.com>
2024-06-08 15:50:44 +0300

    add organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 1),
+(2, 'Rowdy', 10, 'M', 'black', 'aggressive', 1),
+(3, 'Tabitha', 6, 'F', 'light brown', 'friendly', 1),
+(4, 'Sargeant Brown', 19, 'M', 'dark brown', 'aggressive', 0),
+(5, 'Roscoe', 20, 'M', 'black', 'friendly', 0),
+(6, 'Iceman', 12, 'M', 'white', 'aggressive', 1),
+(7, NULL, 2, 'M', 'black', 'friendly', 1),
+(8, 'Kenai', 3, 'M', 'brown', 'friendly', 1);
